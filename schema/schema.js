78283dc23b06e97b3b62780476f657ffbbb9fd11
BillStephens2022 <two4onebill@yahoo.com>
2023-03-14 10:15:14 -0400

    added delete user mutation

--- schema/schema.js
+++ schema/schema.js
@@ -78,9 +78,20 @@ const mutation = new GraphQLObjectType({
           age: { type: new GraphQLNonNull(GraphQLInt) },
           companyId: { type: GraphQLString }
         },
-        resolve(parentValue, { firstName, age }) {
+        resolve(parentValue, { firstName, age, companyId }) {
           return axios
             .post('http://localhost:3000/users', { firstName, age })
+            .then (res => res.data);
+        }
+      },
+      deleteUser: {
+        type: UserType,
+        args: {
+          id: { type: newGraphQLNonNull(GraphQLString) }
+        },
+        resolve(parentValue, { id }) {
+          return axios
+            .delete(`http://localhost:3000/users/${id}`)
             .then (res => res.data);
         }
       }   
